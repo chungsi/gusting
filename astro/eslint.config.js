fd7d2a92c536b4446c21e0cc8c2cf646a3fc9725
ashetea <ashleyctsang@gmail.com>
2024-12-25 10:58:02 -0800

    feat: update configs

--- astro/eslint.config.js
+++ astro/eslint.config.js
@@ -1,11 +1,52 @@
-import pluginJs from "@eslint/js"
+import js from "@eslint/js"
+import eslintPluginAstro from 'eslint-plugin-astro'
+import importPlugin from 'eslint-plugin-import'
 import globals from "globals"
-import tseslint from "typescript-eslint"
+import ts from "typescript-eslint"
 
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: [ '**/*.{ts,tsx,js,jsx,astro}' ] },
   { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  js.configs.recommended,
+  importPlugin.flatConfigs.typescript,
+  ...ts.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  // ...eslintPluginAstro.configs[ 'jsx-a11y-recommended' ],
+  { ignores: [ "**/*.d.ts" ] },
+  {
+    files: [ '**/*.{ts,tsx,astro}' ],
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      'import/newline-after-import': [ 'error', { count: 1, exactCount: false } ],
+      'import/order': [
+        'error', {
+          pathGroups: [
+            { pattern: "@/**.{types,d}.ts", group: 'type' },
+            { pattern: "@/utils/groq/**", group: 'type', position: 'before' },
+            { pattern: '@/**', group: 'internal' },
+            { pattern: "@**/*.astro", group: 'internal' },
+          ],
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+            'object',
+            'type',
+          ],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true
+          },
+          named: false
+        }
+      ]
+    }
+  }
 ]
